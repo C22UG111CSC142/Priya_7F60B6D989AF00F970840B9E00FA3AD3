5ec14fe428e052915369293816f5994a0924cc62
C22UG111CSC142 <144682737+C22UG111CSC142@users.noreply.github.com>
2023-09-22 10:50:19 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,11 +1,17 @@
-# leap year
-def isleapYear(year):
-  if (year % 4 ==0 and year % 100 !=0) or year % 400 == 0:
-    return True
-  else:
-    return False
-year = 2012
-if isleapYear (year):
-  print('{}is a leap year .' .format(year))
-else:
-  print('{} is not a leap year.' .format(year))
+#Here we write a function linear search product that lists product and target product and do the tasks which given in challenge.
+def LinearSearchProduct(productList, targetproduct):
+  indices = []
+
+  for index, product in enumerate(productList):
+    if product == targetproduct:
+      indices.append(index)
+
+  return indices
+
+#Lets check the code with Example
+products = ["shoes","boot","loafer","shoes","sandal","shoes","boot","shoes"]
+target = "shoes"
+target2 = 'apple'
+result = LinearSearchProduct(products,target)
+print(result)
+print("\nLinear Search Performed")
